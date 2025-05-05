2288b10e9ccf279dd94d86ab732e230a1fde0c14
alzaeemraad <alzaeemraad9@gmail.com>
2025-05-06 02:44:04 +0530

    files of resume and profile image

--- src/server.js
+++ src/server.js
@@ -45,7 +45,7 @@ const settingSchema = new mongoose.Schema({
 const Setting = mongoose.model('Setting', settingSchema);
 
 // Ensure uploads folder exists and serve it
-const uploadFolder = path.join(__dirname, 'uploads');
+const uploadFolder = path.join(__dirname, '../uploads');
 if (!fs.existsSync(uploadFolder)) {
   fs.mkdirSync(uploadFolder, { recursive: true });
 }
